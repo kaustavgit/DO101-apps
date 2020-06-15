c01c265c79c9a2f31b154faf054b847653770b64
kaustavgit <66784851+kaustavgit@users.noreply.github.com>
2020-06-15 14:32:45 +0530

    Update app.js
    changes in OCP

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/mars', function (req, res) {
   res.send('Hello World Mars!\n');
 });
 
+app.get('/ocp', function (req, res) {
+  res.send('I am adding it to test that I am able to modify code, re build and deploy to OCP successfully.\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
